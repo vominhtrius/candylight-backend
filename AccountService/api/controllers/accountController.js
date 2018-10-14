daa170a0a5e45bc48ace560a5e663f8388f2245c
nganhtuan <nganhtuan200297@gmail.com>
2018-10-15 00:10:23 +0700

    Complete Get and Update Info

--- AccountService/api/controllers/accountController.js
+++ AccountService/api/controllers/accountController.js
@@ -50,9 +50,84 @@ function signup(req, res) {
   res.json({access_token: accessToken, refesh_token: refreshToken});
 }
 
+//update info user
+function updateInfo(req, res) {
+  var body = req.swagger.params.body;
+  var userId = '1223456'
+  var userName = 'tuan'
+  var pointReward = 0
+  var firstName = body.value.firstName;
+  var lastName = body.value.lastName;
+  var region = body.value.region;
+  var school = body.value.school;
+  var capacity = body.value.capacity;
+  var fNameParent = body.value.firstNameParent;
+  var lNameParent = body.value.lastNameParent;
+  var emailParent = body.value.emailParent;
+  
+  
+  var userInfo = {
+    userId: userId,
+    userName: userName, 
+    firstName: firstName,
+    lastName: lastName,
+    region: region,
+    school: school,
+    capacity: capacity,
+    fNameParent: fNameParent,
+    lNameParent: lNameParent,
+    pointReward: pointReward,
+    emailParent: emailParent
+  };
 
 
+  
+  res.status(200);
+  res.json({success:'OK', profile: userInfo});
+}
+
+//get info user
+function getInfo(req, res) {
+  var userId = '1223456'
+  var userName = 'tuanna'
+  //var passWord = '123456'
+  var firstName = 'tuan'
+  var lastName = 'nguyen'
+  var region = 'HCM'
+  var school = 'HCMUS'
+  var capacity = 'Kha'
+  var fNameParent = 'A'
+  var lNameParent = 'Nguyen'
+  var pointReward = 0
+  var emailParent = 'bnguyen@gmail.com'
+  
+  
+  
+  var userInfo = {
+    userId: userId,
+    userName: userName, 
+    firstName: firstName,
+    lastName: lastName,
+    region: region,
+    school: school,
+    capacity: capacity,
+    fNameParent: fNameParent,
+    lNameParent: lNameParent,
+    pointReward: pointReward,
+    emailParent: emailParent
+  };
+
+  // var user = {
+  //   username: userName, 
+  //   password: passWord,
+  // };
+  
+  res.status(200);
+  res.json({profile: userInfo});
+}
 module.exports = {
   signin: signin,
   signup: signup,
+  updateInfo: updateInfo,
+  getInfo: getInfo,
 };
