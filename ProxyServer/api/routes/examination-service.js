b64b2e04344f2f6ad8d943f0c0c09ffb48dfb645
Tai. Pham Tinh Hong Tan <taiptht@vng.com.vn>
2018-11-28 18:08:52 +0700

    update host proxy server

--- ProxyServer/api/routes/examination-service.js
+++ ProxyServer/api/routes/examination-service.js
@@ -4,6 +4,6 @@ const hostService = require('../../config/config-router.js');
 module.exports = function(app, apiProxy){
     app.all("/api/examination*", function(req, res) {
         console.log(req.method + " " + req.url);
-        apiProxy.web(req, res, {target: hostService.hostAccountService});
+        apiProxy.web(req, res, {target: hostService.hostExamination});
     });    
 }
