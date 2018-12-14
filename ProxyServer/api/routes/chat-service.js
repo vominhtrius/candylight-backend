91120c777ffa8919138e54744d12046b82c08159
Phạm Tĩnh Hồng Tấn Tài <pthtantai97@gmail.com>
2018-12-14 10:33:09 +0700

    fix chat service

--- ProxyServer/api/routes/chat-service.js
+++ ProxyServer/api/routes/chat-service.js
@@ -2,7 +2,7 @@ const hostService = require('../../config/config-router.js');
 
 module.exports = function(app, apiProxy){
     app.all("/api/chat*", function(req, res) {
-        console.log('redirecting to Server1');
+        console.log(req.method + " " + req.url);
         apiProxy.web(req, res, {target: hostService.hostChatService});
     }); 
 }
